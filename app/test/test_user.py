de843fa7c5a27f9093d49b35b97ac61c87128336
NderituMwanu <mwanugerald@gmail.com>
2020-05-11 14:35:48 +0300

    test classes for models

--- app/test/test_user.py
+++ app/test/test_user.py
@@ -6,5 +6,6 @@
 #     def setUp(self):
 #         self.new_user = User(password = 'password')
 
+
 #     def test_password_setter(self):
 #         self.assertTrue(self.new_user.pass_secure is not None)
