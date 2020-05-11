8866b0ed76f5d32c3db6a22a16c6f18b258bb80d
NderituMwanu <mwanugerald@gmail.com>
2020-05-11 14:33:25 +0300

    form classes

--- app/forms.py
+++ app/forms.py
@@ -10,6 +10,7 @@ class PostForm(FlaskForm):
     content = TextAreaField('Content', validators=[DataRequired()])
     submit = SubmitField('Post')
     
+    
 class CommentForm(FlaskForm):
     comment = StringField('Comment')
     submit = SubmitField('Post')
